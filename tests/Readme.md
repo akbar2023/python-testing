ea42a5ce9f65a89b89af0a6564ff5dc17d83db91
akbar <akhan2023@yahoo.com>
2023-12-20 10:24:34 +0100

    initial commit

---
+++ tests/Readme.md
@@ -0,0 +1,10 @@
+pip install pytest
+
+- lancer les tests
+pytest
+
+- Flag verbose (-v) permet d'avoir le mot "PASSED"
+pytest -v
+
+- Flag Statement permet d'avoir les print() / logs
+pytest -s
---
+++ tests/test_101.py
@@ -0,0 +1,14 @@
+
+
+def test_101():
+  print("101 test")
+  assert (100+1 == 101)
+
+def test_102():
+  print("102 test")
+  assert (100+2 == 102)
+
+def test_103():
+  print("103 test")
+  assert (100+3 == 103)
+
